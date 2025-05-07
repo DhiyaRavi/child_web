c2f5f9fb7031d892fbbb043ae3ed7ad0088354ca
DhiyaRavi <sandhiyaravindran5@gmail.com>
2025-05-07 17:59:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,117 @@
+var total=document.querySelectorAll(".alpha").length;
+for(i=0;i<total;i++){
+document.querySelectorAll(".alpha")[i].addEventListener("click",sound)
+function sound(){
+    var buttonClick=this.innerHTML
+    switch(buttonClick)
+    {
+        case "A":
+            var audio=new Audio("A_a.opus");
+            audio.play();
+            break;
+            case "B":
+                var audio=new Audio("B_a.opus");
+                audio.play();
+                break;
+                case "C":
+                    var audio=new Audio("C_a.opus");
+                    audio.play();
+                    break;
+                    case "D":
+                        var audio=new Audio("D_a.opus");
+                        audio.play();
+                        break;
+                        case "E":
+                            var audio=new Audio("E_a.opus");
+                            audio.play();
+                            break;
+                            case "F":
+                                var audio=new Audio("F_a.opus");
+                                audio.play();
+                                break;
+                                case "G":
+                                var audio=new Audio("G_sa.opus");
+                                audio.play();
+                                break;
+                                case "H":
+                                    var audio=new Audio("H_a.opus");
+                                    audio.play();
+                                    break; 
+                                    case "I":
+                                        var audio=new Audio("I_a.opus");
+                                        audio.play();
+                                        break;
+                                        case "J":
+                                        var audio=new Audio("J_a.opus");
+                                        audio.play();
+                                        break;
+                                        case "K":
+                                            var audio=new Audio("K_a.opus");
+                                            audio.play();
+                                            break; 
+                                            case "L":
+                                             var audio=new Audio("L_a.opus");
+                                             audio.play();
+                                              break;
+                                        case "M":
+                                        var audio=new Audio("M_a.opus");
+                                        audio.play();
+                                        break;
+                                        case "N":
+                                        var audio=new Audio("N_a.opus");
+                                        audio.play();
+                                        break;
+                                        case "O":
+                                        var audio=new Audio("O_a.opus");
+                                        audio.play();
+                                        break;
+                                        case "P":
+                                        var audio=new Audio("P_a.opus");
+                                        audio.play();
+                                        break;
+                                        case "Q":
+                                        var audio=new Audio("Q_a.opus");
+                                        audio.play();
+                                        break;
+                                        case "R":
+                                        var audio=new Audio("R_a.opus");
+                                        audio.play();
+                                        break;
+                                        case "S":
+                                        var audio=new Audio("S_a.opus");
+                                        audio.play();
+                                        break;
+                                        case "T":
+                                        var audio=new Audio("tt_a.opus");
+                                        audio.play();
+                                        break;
+                                        case "U":
+                                        var audio=new Audio("U_a.opus");
+                                        audio.play();
+                                        break;
+                                        case "V":
+                                        var audio=new Audio("V_a.opus");
+                                        audio.play();
+                                        break;
+                                        case "W":
+                                        var audio=new Audio("W_a.opus");
+                                        audio.play();
+                                        break;
+                                        case "X":
+                                        var audio=new Audio("X_a.opus");
+                                        audio.play();
+                                        break;
+                                        case "Y":
+                                        var audio=new Audio("Y_a.opus");
+                                        audio.play();
+                                        break;
+                                        case "Z":
+                                        var audio=new Audio("Z_a.opus");
+                                        audio.play();
+                                        break;
+
+    } 
+
+
+    }
+}
